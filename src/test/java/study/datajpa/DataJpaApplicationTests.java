45101caac41b212355920633cab7ef6b3014ab43
audgks5551 <audgks5551@daum.net>
2022-02-03 23:11:15 +0900

    jpa 기본 기능 구현

--- src/test/java/study/datajpa/DataJpaApplicationTests.java
+++ src/test/java/study/datajpa/DataJpaApplicationTests.java
@@ -1,13 +1,14 @@
 package study.datajpa;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberJpaRepository;
 
-@SpringBootTest
+
 class DataJpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
